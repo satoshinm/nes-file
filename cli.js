3ff117dca3a5c9eaa645b80e29ff58ff6b5f490e
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-11-24 15:37:08 -0800

    Add a simple command-line interface

---
+++ cli.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const fs = require('fs');
+const { parse } = require('./');
+
+if (process.argv.length < 3) {
+  process.stderr.write(`NES .nes file parser
+Usage: node cli.js filename
+`);
+  process.exit(1);
+}
+
+const filename = process.argv.slice(2)[0];
+const buf = fs.readFileSync(filename);
+console.log(parse(buf));
